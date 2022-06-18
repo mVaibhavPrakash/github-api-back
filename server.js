6ba5375938e7e62f55f0d3a2860ba9c26c54686a
Vaibhav prakash <vp1997dav@gmail.com>
2022-06-18 21:04:47 +0530

    Final

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 import connectDB from './src/database/database.js';
 import usernameRoute from './src/routes/username.js';
 import allRoute from './src/routes/all.js';
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
-
+/*
 let allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -21,13 +22,24 @@ let allowCrossDomain = function (req, res, next) {
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
-    res.send(200);
+    res.status(200);
   } else {
     next();
   }
 };
 
 app.use(allowCrossDomain);
+
+*/
+
+app.use(urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: 'http://localhost:8081',
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 // mongodb connection
 connectDB();
@@ -40,7 +52,7 @@ app.use(allRoute);
 app.use(barchartRoute);
 
 //Server connection
-const PORT = process.env.PORT || 8082;
+const PORT = 8082;
 app.listen(PORT, () => {
   console.log('Server is connectede');
 });
