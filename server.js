04ca6a775ac7cd71afb9a79501a97048dc41329a
Vaibhav prakash <vp1997dav@gmail.com>
2022-06-18 20:39:59 +0530

    Final

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const app = express();
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
+let allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
   );
-  next();
-});
 
-app.use(
-  cors({
-    origin: [process.env.FRONT, 'http://localhost:8081'],
-    credentials: true,
-  })
-);
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 
 // mongodb connection
 connectDB();
