ac9b734834ddaa5dcf3542d2f2422cc93a5ac3ac
Vaibhav prakash <vp1997dav@gmail.com>
2022-06-18 21:31:04 +0530

    Final

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use(allowCrossDomain);
 */
 
 app.use(urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: 'http://localhost:8081',
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: 'http://localhost:8081',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 // mongodb connection
